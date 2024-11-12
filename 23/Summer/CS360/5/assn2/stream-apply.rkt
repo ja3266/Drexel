8169f6387f7fb816112f041aae48b260a6ac8e8e
Jacob Andrew <jacobandrew2202@gmail.com>
2024-11-12 10:47:52 -0500

    Removing 23 and adding work

--- 23/Summer/CS360/5/assn2/stream-apply.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(provide (all-defined-out))
-
-(define (stream-apply f x)
-	(stream-cons  x (stream-apply f(f x)))
-)
--- 23/Summer/CS360/5/assn2/test-stream-apply.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket
-
-(require "stream-apply.rkt")
-
-(stream->list (stream-take (stream-apply (lambda (x) (+ x 1)) 0) 10))
-
